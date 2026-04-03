09415c8617a02c7d77ac5adc2d788e32847df231
VilliDoug <villidoug@gmail.com>
2026-04-03 21:16:45 +0900

    Postレポジトリー・実装

---
+++ backend/src/main/java/com/devjima/backend/repository/PostRepository.java
@@ -0,0 +1,16 @@
+package com.devjima.backend.repository;
+
+import com.devjima.backend.model.Post;
+import com.devjima.backend.model.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+  List<Post> findByAuthor(User author);
+  List<Post> findByLanguage(String language);
+  List<Post> findByTitleContainingIgnoreCase(String titlePart);
+
+}
