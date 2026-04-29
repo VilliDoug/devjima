32d9ec096ce6697a18668a644040cca14d8a1ee5
VilliDoug <villidoug@gmail.com>
2026-04-29 10:58:26 +0900

    chore: HttpClient設定ファイルを追加

--- backend/src/main/java/com/devjima/backend/config/AppConfig.java
+++ backend/src/main/java/com/devjima/backend/config/AppConfig.java
@@ -1,4 +1,15 @@
 package com.devjima.backend.config;
 
+import java.net.http.HttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+  @Bean
+  public HttpClient httpClient() {
+    return HttpClient.newHttpClient();
+  }
+
 }
