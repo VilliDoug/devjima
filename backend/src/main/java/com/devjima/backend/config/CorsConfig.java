c79fec0972e1a9e1160a9f5058ec4b429cf26257
VilliDoug <villidoug@gmail.com>
2026-04-29 23:10:22 +0900

    fix: CORS設定にGitHubPagesアドレスを追加

--- backend/src/main/java/com/devjima/backend/config/CorsConfig.java
+++ backend/src/main/java/com/devjima/backend/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
     CorsConfiguration config = new CorsConfiguration();
     config.addAllowedOrigin("http://localhost:3000");
     config.addAllowedOrigin("https://devjima-pearl.vercel.app");
+    config.addAllowedOrigin("https://villidoug.github.io");
     config.addAllowedMethod("*");
     config.addAllowedHeader("*");
     config.setAllowCredentials(true);
